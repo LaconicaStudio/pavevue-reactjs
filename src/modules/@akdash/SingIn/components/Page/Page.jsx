f60b13e42d43a8d5510ebc038ecb69110929f3c7
Viktoria <viktoria.k@laconica.com>
2025-08-19 11:46:42 +0300

    sing-in

--- src/modules/@akdash/SingIn/components/Page/Page.jsx
+++ src/modules/@akdash/SingIn/components/Page/Page.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 import Form from "../Form";
+import {Link} from "react-router-dom";
 
 const Page = () => {
     return (
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center w-full p-5">
             <div className="absolute top-0 left-0 right-0 bottom-0 z-[-1]">
                 <img src="/images/bg-image.png" alt="" className="w-full h-full object-cover"/>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 max-w-[1200px] mx-auto">
                 <div></div>
-                <div className="text-left">
+                <div className="text-left max-w-[600px]">
                     <div>
-                        <h1 className="text-2xl mb-4 font-sans-bold lg:text-[44px] lg:leading-[54px] text-white">Let’s sign you in!</h1>
+                        <h1 className="text-2xl mb-4 font-sans-semiBold lg:text-[44px] lg:leading-[54px] text-white">Let’s sign you in!</h1>
                         <Form/>
                     </div>
                     <div>
-                        <h2>
+                        <h2 className="text-gold font-sans-semiBold text-lg lg:text-[34px] lg:leading-[54px] mb-2.5">
                             Don’t have an account?
                         </h2>
-                        <p>
+                        <p className="text-white font-sans-semiBold text-base lg:text-[21px] lg:leading-7  mb-2.5">
                             Get started today and take the first step toward smarter maintenance, fewer surprises, and more money in your pocket.
                         </p>
-                        <a href="">Create My Account</a>
+                        <Link to="/">Create My Account</Link>
                     </div>
                 </div>
             </div>
         </div>
     )
-
-
 }
 
 export default Page;
