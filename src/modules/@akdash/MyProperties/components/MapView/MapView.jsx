746e5db22b942596b74cff81996515ac402ba852
Viktoria <viktoria.k@laconica.com>
2025-09-03 16:42:00 +0300

    AK-1766: add Test Map

--- src/modules/@akdash/MyProperties/components/MapView/MapView.jsx
+++ src/modules/@akdash/MyProperties/components/MapView/MapView.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-
-
+import GoogleMapReact from 'google-map-react';
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const MapView = () => {
+
+    const center = { lat: 10.99835602, lng: 77.01502627 };
 
     // const {tabItems} = useMyProperties();
 
 
     return (
-        <div className="bg-grey w-full  pb-7 px-6">
-MAP
+        <div className="bg-grey w-full relative h-[600px] w-full  pb-7 px-6">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: import.meta.env.VITE_GMAPS_KEY, libraries: ['places'] }}
+                defaultCenter={center}
+                defaultZoom={11}
+            >
+                <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+            </GoogleMapReact>
         </div>
     )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import fs from 'fs'
-//
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     https: {
-//       key: fs.readFileSync('./certs/pavevue.loc+1-key.pem'),
-//       cert: fs.readFileSync('./certs/pavevue.loc+1.pem'),
-//     },
-//     host: 'pavevue.loc',
-//     port: 3000,
-//     cors: true,
-//   },
-// })
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
-   export default defineConfig({
+export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./certs/pavevue.loc-key.pem'),
+      cert: fs.readFileSync('./certs/pavevue.loc.pem'),
+    },
+    host: 'pavevue.loc',
+    port: 3000,
+    cors: true,
+  },
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vite.dev/config/
+//    export default defineConfig({
+//   plugins: [react()],
+// })
